9ebd5a8fb1ee574fac28179cf02176ee8b2ce969
HimNegi18 <negihimu18@gmail.com>
2025-09-02 06:03:23 +0530

    backend path updated

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000', // backend URL
+    baseURL: 'https://todo-6r6w.onrender.com', // backend URL
     withCredentials: true // to send cookies in every request
 })
 
